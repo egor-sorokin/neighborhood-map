3175b93a90d8ec07f82d56817b10e2498577cf99
Egor Sorokin <egorsorokin@Handsomes-MacBook-Pro.local>
2017-09-20 08:23:24 +0600

    Build

--- app/assets/js/google-map.js
+++ app/assets/js/google-map.js
@@ -1,229 +1,81 @@
-var initMap;
-
-(function () {
+(function() {
   'use strict';
-
-  initMap = function () {
-    var map;
-    var locations, markers, mapStyles;
-    var mapContainer, largeInfowindow, defaultMarkerIcon, highlightedMarkerIcon;
-
-    markers = [];
-    mapContainer = document.getElementById('map');
-    largeInfowindow = new google.maps.InfoWindow();
-
-    mapStyles = [
-      {elementType: 'geometry', stylers: [{color: '#7a757b'}]},
-      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-      {elementType: 'labels.text.fill', stylers: [{color: '#d7c2d2'}]},
-      {
-        featureType: 'administrative.locality',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#f0e9ee'}]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d3ccd5'}]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'geometry',
-        stylers: [{color: '#3c5842'}]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#669a5c'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{color: '#ca9bd2'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#352937'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#dbdddd'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{color: '#746855'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#d48dc2'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#f3d19c'}]
-      },
-
-      {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{color: '#9b8d9c'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#8d7985'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.stroke',
-        stylers: [{color: '#a18a98'}]
-      }
-    ];
-
-    locations = [
-      {
-        place_id: 'ChIJ_f9-6SYGelMRaZ4lDCFImk4',
-        title: 'Kootenay National Park',
-        location: {lat: 50.9769367, lng: -115.9592102}
-      },
-      {
-        place_id: 'ChIJyaLFSj48eVMRc4lvx8rhwJ0',
-        title: 'Two Lakes In Revelstoke National Park',
-        location: {lat: 51.0336631, lng: -118.1640469}
-      },
-      {
-        place_id: 'ChIJlZGSjCtmd1MR5tfKrGjincA',
-        title: 'Banff National Park',
-        location: {lat: 51.4968464, lng: -115.9280562}
-      },
-      {
-        place_id: 'ChIJxWd-JkUIeVMRuQ7amxSgRPA',
-        title: 'Glacier National Park of Canada',
-        location: {lat: 51.335289, lng: -117.5297595}
-      },
-      {
-        place_id: 'ChIJv0mRixFDb1MRHFgDVHceYN8',
-        title: 'Waterton Lakes National Park',
-        location: {lat: 49.0833333, lng: -113.9166667}
-      }
-    ];
-
-    map = new google.maps.Map(mapContainer, {
-      center: {lat: 54.367859, lng: -114.266224},
-      zoom: 13,
-      mapTypeControl: false,
-      styles: mapStyles
-    });
+    //
+    // function populateInfoWindow(marker, infowindow) {
+    //   if (infowindow.marker !== marker) {
+    //     infowindow.marker = marker;
+    //     infowindow.setContent('<div>' + marker.position + '</div>');
+    //
+    //     infowindow.addListener('closeclick', function() {
+    //       infowindow.marker = null;
+    //       marker.setIcon(defaultMarkerIcon);
+    //     });
+    //
+    //     var streetViewService = new google.maps.StreetViewService();
+    //     var radius = 50;
+    //
+    //     function getStreetView(data, status) {
+    //       if (status === google.maps.StreetViewStatus.OK) {
+    //         var nearStreetViewLocation = data.location.latLng;
+    //         var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
+    //
+    //         infowindow.setContent('<div class="text">' + marker.title + '</div><div id="pano" class="pano"></div>');
+    //
+    //         var panoramaOptions = {
+    //           position: nearStreetViewLocation,
+    //           pov: {
+    //             heading: heading,
+    //             pitch: 30
+    //           }
+    //         };
+    //         var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+    //       } else {
+    //         infowindow.setContent('<div class="text">' + marker.title + '</div>' +
+    //           '<div>No Street View Found</div>');
+    //       }
+    //     }
+    //
+    //     streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+    //
+    //     infowindow.open(map, marker);
+    //   }
+    // }
 
 
-    function makeMarkerIcon(markerColor) {
-      var markerImage = new google.maps.MarkerImage(
-        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
-        '|40|_|%E2%80%A2',
-        new google.maps.Size(21, 34),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(10, 34),
-        new google.maps.Size(21, 34));
-      return markerImage;
-    }
+    // function initMarkers() {
+    //   for (var i = 0; i < locations.length; i++) {
+    //     var position = locations[i].location;
+    //     var title = locations[i].title;
+    //
+    //     var marker = new google.maps.Marker({
+    //       position: position,
+    //       title: title,
+    //       id: i,
+    //       draggable: true,
+    //       animation: google.maps.Animation.DROP,
+    //       icon: defaultMarkerIcon
+    //     });
+    //
+    //     app.viewModel.placesList()[i].marker = marker;
+    //
+    //     markers.push(marker);
+    //
+    //     marker.addListener('click', function() {
+    //       this.setIcon(highlightedMarkerIcon);
+    //       populateInfoWindow(this, largeInfowindow);
+    //     });
+    //   }
+    // }
 
 
-    defaultMarkerIcon = makeMarkerIcon('0091ff');
-    highlightedMarkerIcon = makeMarkerIcon('f3d19c');
-
-
-    function populateInfoWindow(marker, infowindow) {
-      if (infowindow.marker !== marker) {
-        infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.position + '</div>');
-
-        infowindow.addListener('closeclick', function () {
-          infowindow.marker = null;
-          marker.setIcon(defaultMarkerIcon);
-        });
-
-        var streetViewService = new google.maps.StreetViewService();
-        var radius = 50;
-
-        function getStreetView(data, status) {
-          if (status === google.maps.StreetViewStatus.OK) {
-            var nearStreetViewLocation = data.location.latLng;
-            var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
-
-            infowindow.setContent('<div class="text">' + marker.title + '</div><div id="pano" class="pano"></div>');
-
-            var panoramaOptions = {
-              position: nearStreetViewLocation,
-              pov: {
-                heading: heading,
-                pitch: 30
-              }
-            };
-            var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
-          } else {
-            infowindow.setContent('<div class="text">' + marker.title + '</div>' +
-              '<div>No Street View Found</div>');
-          }
-        }
-
-        streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-
-        infowindow.open(map, marker);
-      }
-    }
-
-
-    function initMarkers() {
-      for (var i = 0; i < locations.length; i++) {
-        var position = locations[i].location;
-        var title = locations[i].title;
-
-        var marker = new google.maps.Marker({
-          position: position,
-          title: title,
-          id: i,
-          draggable: true,
-          animation: google.maps.Animation.DROP,
-          icon: defaultMarkerIcon
-        });
-
-        markers.push(marker);
-
-        marker.addListener('click', function () {
-          this.setIcon(highlightedMarkerIcon);
-          populateInfoWindow(this, largeInfowindow);
-        });
-      }
-    }
-
-
-    function showMarkers() {
-      var bounds = new google.maps.LatLngBounds();
-      for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-        bounds.extend(markers[i].position);
-      }
-
-      map.fitBounds(bounds);
-    }
-
-
-    function setOnGoogleMapLoadEvent() {
-      google.maps.event.addDomListener(window, 'load', function () {
-        var googleMap = initMap();
-
-        ko.applyBindings(new ViewModel(googleMap, locations));
-      });
-    }
-
-
-    initMarkers();
-    showMarkers();
-    setOnGoogleMapLoadEvent();
-  };
+    // function showMarkers() {
+    //   var bounds = new google.maps.LatLngBounds();
+    //   for (var i = 0; i < markers.length; i++) {
+    //     markers[i].setMap(map);
+    //     bounds.extend(markers[i].position);
+    //   }
+    //
+    //   map.fitBounds(bounds);
+    // }
 
 })();
--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,98 +1,325 @@
-// map default binding
-'use strict';
+var app = {} || app;
 
-var Place = function (data) {
-  this.placeId = ko.observable(data.place_id);
-  this.title = ko.observable(data.title);
-  this.latLng = ko.observable(data.latLng);
-  this.marker = null;
-  this.description = ko.observable(data.description);
-  this.itemActive = ko.observable(data.itemActive);
-};
+(function () {
+  'use strict';
 
+  var $ = jQuery;
+  var map;
 
-var ViewModel = function (map, locations) {
-  var self = this;
+  // Main function to init a google map and bind it
+  app.initMap = function () {
+    var mapStyles, mapContainer;
 
-  self.sidebarActiveClass = 'active';
-  self.googleMap = map;
-  self.filterInput = ko.observable('');
-  self.placesList = ko.observableArray([]);
+    mapContainer = document.getElementById('map');
 
-  locations.forEach(function (i) {
-    self.placesList.push(new Place(i));
-  });
-
-  self.currentPlace = ko.observable(self.placesList()[0]);
-
-  self.filter = function () {
-    var inputVal = self.filterInput().toLowerCase();
-    self.placesList([]);
-
-    locations.forEach(function (i) {
-      if (i.title.toLowerCase().indexOf(inputVal) !== -1) {
-        self.placesList.push(new Place(i));
-      }
-    });
-  };
-
-  ko.observableArray.fn.refresh = function (item) {
-    var index = this['indexOf'](item);
-    if (index >= 0) {
-      this.splice(index, 1);
-      this.splice(index, 0, item);
-    }
-  };
-
-  ko.observableArray.fn.refreshAll = function () {
-    var data = this.slice(0);
-    this.removeAll();
-    this(data);
-  };
-
-  self.setCurrentPlace = function (clickedPlace) {
-    self.currentPlace(clickedPlace);
-
-    ko.utils.arrayForEach(self.placesList(), function (item) {
-      item.itemActive = ko.pureComputed(function () {
-        return item.placeId() === clickedPlace.placeId();
-      });
-    });
-
-    self.placesList.refreshAll();
-
-
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + clickedPlace.title() + '&format=json&callback=wikiCallback';
-
-    var wikiRequestTimeout = setTimeout(function () {
-      clickedPlace.description = ko.pureComputed(function () {
-        return 'Failed to get Wikipedia resources';
-      });
-    }, 5000);
-
-    $.ajax({
-      url: wikiUrl,
-      dataType: 'jsonp',
-      jsonp: 'callback',
-      success: function (response) {
-        var description = response[2];
-
-        clickedPlace.description = ko.pureComputed(function () {
-          return description[0] ? description[0] : 'Description not found';
-        });
-
-        self.placesList.refresh(clickedPlace);
-
-        clearTimeout(wikiRequestTimeout);
+    // styles for map
+    mapStyles = [{
+      elementType: 'geometry',
+      stylers: [{
+        color: '#7a757b'
+      }]
+    },
+      {
+        elementType: 'labels.text.stroke',
+        stylers: [{
+          color: '#242f3e'
+        }]
       },
-      error: function (error) {
-        clickedPlace.description = ko.pureComputed(function () {
-          return 'Something went wrong';
-        });
+      {
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#d7c2d2'
+        }]
+      },
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#f0e9ee'
+        }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#d3ccd5'
+        }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#3c5842'
+        }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#669a5c'
+        }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#ca9bd2'
+        }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{
+          color: '#352937'
+        }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#dbdddd'
+        }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#746855'
+        }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{
+          color: '#d48dc2'
+        }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#f3d19c'
+        }]
+      },
 
-        console.log('Request returned an error: ', error);
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#9b8d9c'
+        }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#8d7985'
+        }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{
+          color: '#a18a98'
+        }]
       }
+    ];
+
+    // map initialising
+    map = new google.maps.Map(mapContainer, {
+      center: {
+        lat: 50.467859,
+        lng: -116.266224
+      },
+      zoom: 7,
+      mapTypeControl: false,
+      styles: mapStyles
     });
+
+    // default set of locations
+    var locations = [{
+      place_id: 'ChIJ_f9-6SYGelMRaZ4lDCFImk4',
+      title: 'Kootenay National Park',
+      location: {
+        lat: 50.9769367,
+        lng: -115.9592102
+      }
+    },
+      {
+        place_id: 'ChIJyaLFSj48eVMRc4lvx8rhwJ0',
+        title: 'Two Lakes In Revelstoke National Park',
+        location: {
+          lat: 51.0336631,
+          lng: -118.1640469
+        }
+      },
+      {
+        place_id: 'ChIJlZGSjCtmd1MR5tfKrGjincA',
+        title: 'Banff National Park',
+        location: {
+          lat: 51.4968464,
+          lng: -115.9280562
+        }
+      },
+      {
+        place_id: 'ChIJxWd-JkUIeVMRuQ7amxSgRPA',
+        title: 'Glacier National Park of Canada',
+        location: {
+          lat: 51.335289,
+          lng: -117.5297595
+        }
+      },
+      {
+        place_id: 'ChIJv0mRixFDb1MRHFgDVHceYN8',
+        title: 'Waterton Lakes National Park',
+        location: {
+          lat: 49.0833333,
+          lng: -113.9166667
+        }
+      }
+    ];
+
+    var viewModel = new ViewModel(locations);
+
+    ko.applyBindings(viewModel);
   };
 
-};
+
+  // Place - a description of each place instance
+  var Place = function (data) {
+    this.placeId = ko.observable(data.place_id);
+    this.title = ko.observable(data.title);
+    this.location = ko.observable(data.location);
+    this.marker = ko.observable();
+    this.description = ko.observable(data.description);
+    this.itemActive = ko.observable(false);
+  };
+
+
+  var ViewModel = function (locations) {
+    var self = this;
+    var infowindow = new google.maps.InfoWindow();
+    var marker;
+    var defaultMarkerIconColor;
+
+
+    // Create custom marker icon
+    function makeMarkerIcon(markerColor) {
+      var markerImage = new google.maps.MarkerImage(
+        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+        '|40|_|%E2%80%A2',
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34),
+        new google.maps.Size(21, 34));
+
+      return markerImage;
+    }
+
+    defaultMarkerIconColor = makeMarkerIcon('0091ff');
+
+
+    self.sidebarActiveClass = 'active';
+    self.googleMap = map;
+    self.filterInput = ko.observable('');
+    self.placesList = ko.observableArray([]);
+    self.filterInput = ko.observable('');
+    self.visiblePlacesList = ko.observableArray();
+    self.toggleSidebar = ko.observable(false);
+
+
+    // Fill the places array
+    locations.forEach(function (i) {
+      self.placesList.push(new Place(i));
+    });
+
+
+    // Initially all markers are visible
+    self.placesList().forEach(function (place) {
+      self.visiblePlacesList.push(place);
+    });
+
+
+    // Add a description, a marker and a click event handler to each place
+    self.placesList().forEach(function (place) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(place.location().lat, place.location().lng),
+        map: map,
+        animation: google.maps.Animation.DROP,
+        icon: defaultMarkerIconColor
+      });
+
+      place.marker = marker;
+
+      var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + place.title() + '&format=json&callback=wikiCallback';
+
+      // Request description of each place through Wikipedia API
+      $.ajax({
+        url: wikiUrl,
+        dataType: 'jsonp',
+        jsonp: 'callback',
+        success: function (response) {
+          var description = response[2][0] || 'Description not found';
+
+          place.description(description);
+
+          var infowindowContent = '<div class="info-window">' +
+            '<h4 class="info-window__title">' + place.title() + '</h4>' +
+            '<p class="info-window__description">' + place.description() + '</p></div>';
+
+          google.maps.event.addListener(place.marker, 'click', function () {
+            infowindow.open(map, this);
+            this.setAnimation(google.maps.Animation.DROP);
+            infowindow.setContent(infowindowContent);
+            map.setCenter(place.marker.getPosition());
+          });
+        },
+        error: function () {
+          infowindow.setContent('<p>Failed to get Wikipedia resources</p>');
+          document.getElementById('error-message').innerHTML = '<p>Failed to get Wikipedia resources</p>';
+        }
+      });
+    });
+
+
+    // Handler for showing place description
+    self.showPlaceDescription = function (place) {
+      google.maps.event.trigger(place.marker, 'click');
+
+      ko.utils.arrayForEach(self.visiblePlacesList(), function (item) {
+        item.itemActive(item.placeId() === place.placeId());
+      });
+    };
+
+
+    // Handler for hiding/showing a sidebar
+    self.toggleSidebarHandler = function () {
+      if (self.toggleSidebar()) {
+        self.toggleSidebar(false);
+      } else {
+        self.toggleSidebar(true);
+      }
+    };
+
+
+    // Filter all markers and places, remove all of them and then re-push only visible ones
+    self.filterMarkers = function () {
+      var searchInput = self.filterInput().toLowerCase();
+
+      self.visiblePlacesList.removeAll();
+
+      self.placesList().forEach(function (place) {
+        place.marker.setVisible(false);
+
+        if (place.title().toLowerCase().indexOf(searchInput) !== -1) {
+          self.visiblePlacesList.push(place);
+        }
+      });
+
+      self.visiblePlacesList().forEach(function (place) {
+        place.marker.setVisible(true);
+      });
+    };
+  };
+
+})();
